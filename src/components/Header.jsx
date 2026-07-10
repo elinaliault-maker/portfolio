7ed6e6bb0aeaed270106e2f9ecb3f7a77024285a
Elina Liault <elina.liault@gmail.com>
2026-07-10 16:27:39 +0200

    adding tailwind and begining of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import { NavLink, Link } from "react-router";
+import Logo from "../assets/logo-portfolio-el.svg"
+
+function HeaderLink({ text, url }) {
+    return (
+        <NavLink to={url} className="underline">{text}</NavLink>
+    )
+}
 
 export default function Header() {
     return (
-        <div>
+        <div className="flex gap-4">
             <NavLink>Projets</NavLink>
+            <HeaderLink text="Projets" url="/projets" />
             <NavLink>Garden</NavLink>
-            <Link to="/" className='back-home'>Logo/Home</Link>
+            <Link to="/" className='back-home'>
+                <img src={Logo} alt="Logo" className="w-16" />
+            </Link>
             <NavLink>About</NavLink>
             <NavLink>Contact</NavLink>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 // import './App.css'
+import '../tailwind-import.css'
+import '../index.css'
 import Header from "../components/Header"
 
 function App() {
