89d5de5dce2ba5c4344b0a57a579feeb4864a996
Elina Liault <elina.liault@gmail.com>
2026-07-22 11:31:04 +0200

    Tailwind class correction

--- src/components/PageTitleWithIcons.jsx
+++ src/components/PageTitleWithIcons.jsx
@@ -2,7 +2,7 @@ export default function PageTitleWithIcons({text}) {
     return (
         <div className="relative inline-block mt-4 mb-8 px-16">
             <span 
-                className="font-icon text-2xl text-(--bleu-clair) absolute top-[22px] left-[34px] animate-spin-slow"
+                className="font-icon text-2xl text-(--bleu-clair) absolute top-5.5 left-8.5 animate-spin-slow"
                 aria-hidden="true"
             >
                 {"\uf003"}
@@ -11,7 +11,7 @@ export default function PageTitleWithIcons({text}) {
                 {text}
             </h1>
             <span 
-                className="font-icon text-2xl text-(--violet) absolute bottom-[24px] right-[28px] animate-spin-slow"
+                className="font-icon text-2xl text-(--violet) absolute bottom-6 right-7 animate-spin-slow"
                 style={{ animationDelay: '0.5s' }}
                 aria-hidden="true"
             >
