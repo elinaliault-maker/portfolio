9ce99a6027e994c54ce545930e7fa07deba7cbba
Elina Liault <elina.liault@gmail.com>
2026-08-16 19:06:09 +0200

    Project cover hover try with saturate()

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -17,12 +17,12 @@ function ProjectBlock ({ langUrl, projectUrl, coverUrl, title, detail, types }) 
             rounded-[1px] border-2 border-(--dark-blue) rotate-359"
         >
             <div className="w-auto aspect-3/2 overflow-hidden relative">
-                <div className="hidden group-hover:block bg-[#AA74FB] opacity-40 mix-blend-multiply z-1
+                <div className="hidden group-hover:block bg-[#AA74FB] opacity-60 mix-blend-color z-1
                 w-full h-full absolute top-0 left-0"></div>
                 <div 
                     style={{ backgroundImage: `url(${coverUrl})` }} 
                     className="w-full h-full bg-cover bg-center 
-                    transition-transform duration-300 ease-in-out group-hover:scale-105 group-hover:mix-blend-luminosity"
+                    transition-transform duration-300 ease-in-out group-hover:scale-105 group-hover:saturate-0"
                 />
             </div>
             <div className="flex flex-col gap-1.5 justify-start
